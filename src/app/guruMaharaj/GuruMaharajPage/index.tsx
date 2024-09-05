bdccefab38748fe6c47b75b8ae70a1a4f3e38ac8
Aleksei M <lexmonakhov@gmail.com>
2024-09-05 10:43:41 +0545

    add guruMaharajPage container, swiper, styles

--- src/app/guruMaharaj/GuruMaharajPage/index.tsx
+++ src/app/guruMaharaj/GuruMaharajPage/index.tsx
@@ -3,13 +3,21 @@ import React, { FC } from 'react';
 import styles from './styles.module.scss';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import Title from '@/components/Title';
+import Swiper from '@/components/Swiper';
+import { useImages } from '@/hooks/useImages';
+
+
 
 const GuruMaharajPage: FC = () => {
- 
+  const { images: swiperNaraharinath } = useImages('Naraharinath');
   return (
     <>
       <Header />
-      
+      <div className={styles.container}>
+      <Title mainTitle='Guru Naraharinath Maharaj' />
+      <Swiper properties={swiperNaraharinath} />
+      </div>
       <Footer />
     </>
   );
